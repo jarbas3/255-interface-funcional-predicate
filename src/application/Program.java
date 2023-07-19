8f1b1b97ad7cf4d2713b4bb019cb1122fae4dc62
Jarbas Santos <jarbas3@outlook.com>
2023-07-19 16:32:26 -0300

    Expressão lambda inline

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.function.Predicate;
 
 import entities.Product;
@@ -10,15 +11,15 @@ public class Program {
 	
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		
 		List<Product> list = new ArrayList<>();
 		list.add(new Product("Tv", 900.00));
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		Predicate<Product> pred = p -> p.getPrice() >= 100.0;
-		
-		list.removeIf(pred);
+		list.removeIf(p -> p.getPrice() >= 100.0);
 		list.forEach(System.out::println);
 	}
 }
